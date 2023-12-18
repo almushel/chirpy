539017a1d4351437d9aea776467a1b03bbc13055
Andrew Mushel <almushel@protonmail.com>
2023-12-18 12:13:21 -0600

    Routing

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+type apiConfig struct {
+	filerserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.filerserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Content-type: text/plain; charset=utf8"))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.filerserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.filerserverHits = 0
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,19 +42,24 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	w.Write([]byte("Centent-type: text/plain; charset=utf8"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Content-type: text/plain; charset=utf8"))
 	w.Write([]byte("OK"))
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", healthzHandler)
-	corsMux := middlewareCors(mux)
+	cfg := new(apiConfig)
+	fs := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r := chi.NewRouter()
+	r.Handle("/app/*", fs)
+	r.Handle("/app", fs)
+	r.Get("/healthz", healthzHandler)
+	r.Get("/metrics", cfg.metricsHandler)
+	r.HandleFunc("/reset", cfg.resetHandler)
+	corsr := middlewareCors(r)
 
 	var server http.Server
-	server.Handler = corsMux
+	server.Handler = corsr
 	server.Addr = "localhost:8080"
 	log.Println("Chirpy listening and serving at", server.Addr)
 	log.Fatalf(server.ListenAndServe().Error())
